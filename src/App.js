9734df9a30c489c808c76270e8c7edda0c2c537a
yogeshsurapaneni <yogeshsurapaneni@gmail.com>
2023-05-04 14:16:51 +0000

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 function App() {
   
-  const [data, setData] = useState([{}]) 
+  const [data, setData] = useState([]) 
 
   useEffect(() => {
       fetch("/host").then(
@@ -10,20 +10,17 @@ function App() {
       ).then(
         data => {
             setData(data)
-            console.log(data)
         }
       )
   }, [])
 
-return (
-  <body>
-        <h1 style={{fontWeight: 'bold', color: 'red' }}> hostname </h1>
-        <p>{data.host[0]}</p>
+  return (
+    <div className="App">
+          <h1 style={{fontWeight: 'bold', color: 'red' }}> hostname: </h1> <p>{data.hostname}</p>
+          <h1 style={{fontWeight: 'bold', color: 'red' }}> Free Memory Percent: </h1> <p>{data.ram}</p>
 
-        <h1 style={{fontWeight: 'bold', color: 'red' }}> Free Memory Percent </h1>
-        <p>{data.host[1]}</p>  
-  </body>
-)
-}
-
-export default App
+    </div>      
+  
+  )
+  }
+  export default App
